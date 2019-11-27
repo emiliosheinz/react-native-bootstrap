f523d1e021d5b2df0346e5846209895e97c6f662
emilioheinz <emiliosheinz@gmail.com>
2019-11-26 21:24:13 -0300

    Fixing async storage

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -1,11 +1,11 @@
-import storage from 'redux-persist/lib/storage'
+import AsyncStorage from '@react-native-community/async-storage'
 import { persistReducer } from 'redux-persist'
 
 export default reducers => {
   const persistedReducers = persistReducer(
     {
       key: 'react-native-bootstrap',
-      storage,
+      storage: AsyncStorage,
       whitelist: [
         /* Names of the reducers that you want to persist */
       ],
